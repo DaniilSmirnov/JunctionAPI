cb7498244809be49aa40912f5757f514de9596fa
DaniilSmirnov <littlebigcrafting@gmail.com>
2019-11-15 20:53:54 +0300

    GetWishlists

--- app.py
+++ app.py
@@ -1,12 +1,73 @@
 from flask import Flask
+from flask_restful import Resource, Api, reqparse
+from flask import request
+import mysql.connector
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 
+app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin: *'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+cors = CORS(app)
+api = Api(app)
 
+cnx = mysql.connector.connect(user='root', password='i130813',
+                                  host='127.0.0.1',
+                                  database='junction')
+
+
+class TestConnection(Resource):
+    def get(self):
+        return {'status': 'success'}
+
+
+class GetWishlists(Resource):
+    def get(self):
+        cursor = cnx.cursor()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=int)
+        args = parser.parse_args()
+
+        user_id = args['user_id']
+
+        query = "select * from wishlists where iduser = %s;"
+        data = (user_id, )
+        cursor.execute(query, data)
+        wishlist = {}
+        responce = []
+        for item in cursor:
+            i = 0
+            for value in item:
+                if i == 0:
+                    wishlist.update({'id': value})
+                    id = value
+                if i == 1:
+                    wishlist.update({'user_id': value})
+                if i == 2:
+                    wishlist.update({'name': value})
+
+                products = []
+
+                query = "select id product from products where idwishlist = %s;"
+                data = (id, )
+                cursor.execute(query, data)
+                for item in cursor:
+                    for value in item:
+                        products.append(value)
+
+                wishlist.update({'products': products})
+
+                i += 1
+
+                responce.append(wishlist)
+
+        return responce
+
+
+api.add_resource(TestConnection, '/TestConnection')
+api.add_resource(GetWishlists, '/GetWishlists')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
