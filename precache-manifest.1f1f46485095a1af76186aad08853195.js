e4cdfdaa72aa4c7ce5bf3228c2f5902554889764
n.landa <n.landa@corp.vk.com>
2019-11-16 03:46:29 +0200

    Updates

---
+++ precache-manifest.1f1f46485095a1af76186aad08853195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9ec3c4cb20e2a3582c6fe2f7a014c5",
+    "url": "/JunctionAPI/index.html"
+  },
+  {
+    "revision": "43e3545b5a5e7dd0774b",
+    "url": "/JunctionAPI/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "253a5939323881850746",
+    "url": "/JunctionAPI/static/css/main.3754646a.chunk.css"
+  },
+  {
+    "revision": "43e3545b5a5e7dd0774b",
+    "url": "/JunctionAPI/static/js/2.fd5cf386.chunk.js"
+  },
+  {
+    "revision": "253a5939323881850746",
+    "url": "/JunctionAPI/static/js/main.2bc5c681.chunk.js"
+  },
+  {
+    "revision": "da284a8caca1fb5e6cb0",
+    "url": "/JunctionAPI/static/js/runtime-main.b9f9f29a.js"
+  }
+]);
